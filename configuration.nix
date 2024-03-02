ce31b0c806c548f74172799922ada5212f2eba10
System administrator <moritz.noebauer@gmail.com>
2024-03-02 18:43:28 +0100

    Add nix formatter

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,14 @@
 
 { config, lib, pkgs, ... }:
 
+let
+  alejandra =
+    (import (builtins.fetchTarball {
+      url = "https://github.com/kamadorueda/alejandra/tarball/3.0.0";
+      sha256 = "18jm0d5xrxk38hw5sa470zgfz9xzdcyaskjhgjwhnmzd5fgacny4";
+    }) {})
+    .outPath;
+in
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -128,6 +136,7 @@
     lm_sensors
     git
     alacritty
+    alejandra
     gcc
     clang
     glib.bin
