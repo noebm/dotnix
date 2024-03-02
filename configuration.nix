75a78764848e7a3230d48739781a0fa7bf7e62a6
System administrator <moritz.noebauer@gmail.com>
2024-03-02 13:19:42 +0100

    Enable flakes

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,9 @@
       ./hardware-configuration.nix
     ];
 
+  # enable `nix flake ...` commands
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
