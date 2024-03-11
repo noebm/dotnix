dfd4ab350cf8a27348ebeb90c4b468d86eeda876
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-03-11 11:02:07 +0100

    Set nixvim as default editor

--- configuration.nix
+++ configuration.nix
@@ -29,6 +29,8 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
+  programs.nixvim.defaultEditor = true;
+
   networking.firewall = {
     enable = true;
     allowedTCPPortRanges = [
