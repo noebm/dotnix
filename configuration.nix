937753e4e0ce5a36370dc216b06f44ad178617a0
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-03-12 22:58:39 +0100

    Set neovim as default editor

--- configuration.nix
+++ configuration.nix
@@ -45,6 +45,9 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
+
+  programs.neovim.defaultEditor = true;
+
   # console = {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
