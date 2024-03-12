074c476d52b834081d3b8811b291d96e3f58c573
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-03-12 12:23:38 +0100

    nvim: Simplify completion keybinds

--- config/nvim.nix
+++ config/nvim.nix
@@ -62,25 +62,10 @@
         mapping = {
           "<CR>" = "cmp.mapping.confirm({ select = true })";
           "<Tab>" = {
-            action = ''
-              function(fallback)
-                if cmp.visible() then
-                  cmp.select_next_item()
-                elseif luasnip.expandable() then
-                  luasnip.expand()
-                elseif luasnip.expand_or_jumpable() then
-                  luasnip.expand_or_jump()
-                elseif check_backspace() then
-                  fallback()
-                else
-                  fallback()
-                end
-              end
-            '';
-            modes = [
-              "i"
-              "s"
-            ];
+            action = ''cmp.mapping.select_next_item()'';
+          };
+          "<S-Tab>" = {
+            action = ''cmp.mapping.select_prev_item()'';
           };
         };
       };
@@ -99,6 +84,7 @@
         withArgs = " {\"--profile\", \"black\"} ";
       };
       sources.formatting.alejandra.enable = true;
+      sources.formatting.rustfmt.enable = true;
     };
 
     plugins.lsp = {
