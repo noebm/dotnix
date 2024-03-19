bdcdd5ca9b69897a0f2bd2673f53b4ccc0e01e37
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-03-19 23:11:43 +0100

    fix locale error in logitech service

--- modules/logitech.nix
+++ modules/logitech.nix
@@ -10,5 +10,8 @@
     enable = true;
     script = "${pkgs.solaar}/bin/solaar -w hide";
     wantedBy = ["multi-user.target"];
+
+    # fix locale error in C library
+    environment.LC_ALL = "C";
   };
 }
