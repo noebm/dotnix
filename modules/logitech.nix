3e96bd4c046e0d3e37445ca125bbe4263dca468c
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-03-19 22:31:34 +0100

    add user service for solaar

--- modules/logitech.nix
+++ modules/logitech.nix
@@ -4,4 +4,11 @@
     # solaar
     enableGraphical = true;
   };
+
+  systemd.user.services.logitech = {
+    description = "user daemon for configuration of logitech devices";
+    enable = true;
+    script = "${pkgs.solaar}/bin/solaar -w hide";
+    wantedBy = ["multi-user.target"];
+  };
 }
