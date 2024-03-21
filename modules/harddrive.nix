b56a2ab40e0b19efee95dc5d8e9ade17fed39f5d
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-03-21 15:32:42 +0100

    update / enable hd-idle service

--- modules/harddrive.nix
+++ modules/harddrive.nix
@@ -4,7 +4,7 @@
     wantedBy = ["multi-user.target"];
     serviceConfig = {
       Type = "forking";
-      ExecStart = "${pkgs.hd-idle}/bin/hd-idle -i 0 -a sdb -i 600";
+      ExecStart = "${pkgs.hd-idle}/bin/hd-idle -i 300";
     };
   };
 }
