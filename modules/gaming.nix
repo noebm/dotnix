1fb12e2fe6cd1f419bf2f9d121244252cba28fd4
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-05-06 22:29:14 +0200

    use steam options etc.

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,6 +1,26 @@
 {pkgs, ...}: {
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraPkgs = pkgs:
+        with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+        ];
+    };
+  };
+
   programs.steam.enable = true;
-  environment.systemPackages = with pkgs; [
-    gamescope
-  ];
+  programs.steam.gamescopeSession.enable = true;
+  programs.gamescope.enable = true;
+  # environment.systemPackages = with pkgs; [
+  #   gamescope
+  # ];
 }
