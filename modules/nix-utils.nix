b559fdf8f39588702f20e91548a0168ca514cfb3
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-05-11 01:36:48 +0200

    add nvd and `nix-diff` alias

--- modules/nix-utils.nix
+++ modules/nix-utils.nix
@@ -1,7 +1,13 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     nix-search-cli
+    nvd
   ];
+
+  # cant remember nvd
+  environment.interactiveShellInit = ''
+    alias nix-diff='nvd'
+  '';
 
   nix.optimise = {
     automatic = true;
