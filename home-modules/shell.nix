7786e0d014dee353a388766c59d445b153ecb159
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-05-21 20:21:49 +0200

    feat(prompt): update starship configuration

--- home-modules/shell.nix
+++ home-modules/shell.nix
@@ -1,4 +1,4 @@
-{...}: {
+{lib, ...}: {
   programs.bash.enable = true;
 
   programs.starship.enableBashIntegration = true;
@@ -7,11 +7,28 @@
 
     settings = {
       add_newline = false;
-      # format = "$username$hostname$directory$git_branch$git_commit$git_state$git_status$character";
+      # "$username$hostname$directory$git_branch$git_commit$git_state$git_status$character";
+
+      directory = {
+        truncation_symbol = ".../";
+        truncate_to_repo = false;
+      };
+
+      # git_branch = {
+      #   format = "[@\\[](grey dimmed)[$symbol$branch($remote_branch)]($style)";
+      # };
+
+      # git_status = {
+      #   format = "([\\[$all_status$ahead_behind\\]]($style) )[\\]](grey dimmed)";
+      # };
 
       character = {
         success_symbol = "[>](bright-blue bold)";
         error_symbol = "[x](bright-red bold)";
+      };
+
+      package = {
+        disabled = true;
       };
     };
   };
