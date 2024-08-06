4f5e6a7fa8f160310bc3424b9c1be3b72a927919
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-08-06 15:24:48 +0200

    fix: set xdg associations for firefox

--- home-modules/firefox.nix
+++ home-modules/firefox.nix
@@ -15,4 +15,16 @@
       };
     };
   };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "default-web-browser" = ["firefox.desktop"];
+      "text/html" = ["firefox.desktop"];
+      "x-scheme-handler/http" = ["firefox.desktop"];
+      "x-scheme-handler/https" = ["firefox.desktop"];
+      "x-scheme-handler/about" = ["firefox.desktop"];
+      "x-scheme-handler/unknown" = ["firefox.desktop"];
+    };
+  };
 }
