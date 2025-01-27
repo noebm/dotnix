38c1e4c78cefd7018bbc606cbd905a0c15207770
Moritz Noebauer <moritz.noebauer@gmail.com>
2025-01-27 01:25:45 +0100

    feat(ollama): update models and use open-webui

--- modules/nixos/ollama.nix
+++ modules/nixos/ollama.nix
@@ -3,8 +3,16 @@
     enable = true;
 
     loadModels = [
-      "qwen2.5-coder"
-      "deepseek-r1:7b"
+      "qwen2.5-coder:3b"
+      "qwen2.5-coder:7b"
+      "qwen2.5-coder:14b"
+      "deepseek-r1:8b"
+      "deepseek-r1:14b"
     ];
+  };
+
+  services.open-webui = {
+    enable = true;
+    environment.OLLAMA_API_BASE_URL = "http://localhost:11434";
   };
 }
