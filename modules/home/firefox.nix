105c8e53d599133532280d8031abb000e23dc25e
Moritz Noebauer <moritz.noebauer@gmail.com>
2025-02-16 14:44:10 +0100

    feat(home/firefox): add custom search engines
    - Added custom search engines for Nix Packages, NixOS Modules, NixOS Wiki, and Perplexity.
    - Configured DuckDuckGo as the default search engine and set the search order.
    - Hid default search engines Bing and Ecosia.
    - Enabled forced search configuration.
    
    style(home/firefox): improve formatting
    
    - Reformatted xdg.mimeApps defaultApplications list for consistency.

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -1,4 +1,5 @@
-{...}: {
+{ pkgs, ... }:
+{
   programs.firefox = {
     enable = true;
     profiles.default = {
@@ -13,18 +14,101 @@
         "browser.urlbar.showSearchSuggestionsFirst" = false;
         "intl.accept_languages" = "en,en-us,de";
       };
+
+      search = {
+        force = true;
+
+        default = "DuckDuckGo";
+        order = [
+          "DuckDuckGo"
+          "Google"
+        ];
+
+        engines = {
+          "Bing".metadata.hidden = true;
+          "Ecosia".metadata.hidden = true;
+
+          "Nix Packages" = {
+            urls = [
+              {
+                template = "https://search.nixos.org/packages";
+                params = [
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@nix" ];
+          };
+          "NixOS Modules" = {
+            urls = [
+              {
+                template = "https://search.nixos.org/options";
+                params = [
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@nixos" ];
+          };
+          "NixOS Wiki" = {
+            urls = [
+              {
+                template = "https://nixos.wiki/index.php";
+                params = [
+                  {
+                    name = "search";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@nixwiki" ];
+          };
+
+          "Perplexity" = {
+            urls = [
+              {
+                template = "https://www.perplexity.ai/search/new";
+                params = [
+                  {
+                    name = "q";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+
+            icon = "${pkgs.fetchurl {
+              url = "https://www.perplexity.ai/favicon.ico";
+              hash = "sha256-35pkeFkmYAd+TndNN7rm2x6019nSysMfDc2riAT3bAY=";
+            }}";
+            definedAliases = [ "@perp" ];
+
+          };
+        };
+
+      };
     };
   };
 
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "default-web-browser" = ["firefox.desktop"];
-      "text/html" = ["firefox.desktop"];
-      "x-scheme-handler/http" = ["firefox.desktop"];
-      "x-scheme-handler/https" = ["firefox.desktop"];
-      "x-scheme-handler/about" = ["firefox.desktop"];
-      "x-scheme-handler/unknown" = ["firefox.desktop"];
+      "default-web-browser" = [ "firefox.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/about" = [ "firefox.desktop" ];
+      "x-scheme-handler/unknown" = [ "firefox.desktop" ];
     };
   };
 }
