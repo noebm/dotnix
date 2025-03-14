000c86b9ea7fc1bb2f0572787971dda4d17d2adc
Moritz Noebauer <moritz.noebauer@gmail.com>
2025-03-14 15:29:14 +0100

    refactor: simplify steam setup

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -5,24 +5,6 @@
   ...
 }:
 {
-  nixpkgs.config.packageOverrides = pkgs: {
-    steam = pkgs.steam.override {
-      extraPkgs =
-        pkgs: with pkgs; [
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXinerama
-          xorg.libXScrnSaver
-          libpng
-          libpulseaudio
-          libvorbis
-          stdenv.cc.cc.lib
-          libkrb5
-          keyutils
-        ];
-    };
-  };
-
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true;
   programs.gamescope.enable = true;
