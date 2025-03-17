79b625a241aebdb218bef30f342dcd9595330051
Moritz Noebauer <moritz.noebauer@gmail.com>
2025-03-18 00:24:15 +0100

    update gaming.nix

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -8,12 +8,11 @@
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true;
   programs.gamescope.enable = true;
-  # environment.systemPackages = with pkgs; [
-  #   gamescope
-  # ];
+
+  environment.systemPackages = with pkgs; [ torzu ];
 
   # minecraft
-  environment.systemPackages = [
-    pkgs.prismlauncher
-  ];
+  # environment.systemPackages = [
+  #   pkgs.prismlauncher
+  # ];
 }
