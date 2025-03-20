66c306dfb371a978f8a490af1d8aac6971573fbd
Moritz Noebauer <moritz.noebauer@gmail.com>
2025-03-20 22:41:18 +0100

    upgrade from github repo

--- modules/nixos/upgrade.nix
+++ modules/nixos/upgrade.nix
@@ -3,11 +3,10 @@
 
   system.autoUpgrade = {
     enable = true;
-    flake = inputs.self.outPath;
+    flake = "github:noebm/dotnix";
+    # use package versions in the repo since there is a already an upgrade action running
     flags = [
-      "--update-input"
-      "nixpkgs"
-      "--commit-lock-file"
+      "--no-write-lock-file"
     ];
     dates = "14:00";
     randomizedDelaySec = "45min";
